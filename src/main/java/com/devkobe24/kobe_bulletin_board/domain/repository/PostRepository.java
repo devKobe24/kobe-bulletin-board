2431a861c12f350ae7694cba1c17dc25e2fc404d
Kobe <dev.skyachieve91@gmail.com>
2024-12-16 13:36:12 +0900

    feat: PostRepository 인터페이스 생성 및 구현

---
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.devkobe24.kobe_bulletin_board.domain.repository;
+
+import com.devkobe24.kobe_bulletin_board.domain.repository.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+	Optional<Post> findByTitle(String title);
+}
