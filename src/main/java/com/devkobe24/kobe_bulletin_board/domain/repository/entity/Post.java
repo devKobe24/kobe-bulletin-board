44d17242cc7701310a5dbccac40dcc1a661ecb20
Kobe <dev.skyachieve91@gmail.com>
2024-12-16 13:35:44 +0900

    feat: Post 클래스 생성 및 구현.

---
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/entity/Post.java
@@ -0,0 +1,39 @@
+package com.devkobe24.kobe_bulletin_board.domain.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "post")
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String content;
+
+	@Setter
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "writer", referencedColumnName = "nick_name", nullable = false)
+	private User user;
+
+	@Column(name = "created_at")
+	private Timestamp createdAt;
+
+	@Column(name = "view_count")
+	private Integer viewCount;
+
+	@Column(nullable = false, length = 60)
+	private String password;
+}
