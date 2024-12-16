751c312c2bfc5c63d9d9a3603c1cd079f46bee9c
Kobe <dev.skyachieve91@gmail.com>
2024-12-16 16:39:49 +0900

    feat: PostReadService 클래스 생성

---
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/post/service/PostReadService.java
@@ -0,0 +1,43 @@
+package com.devkobe24.kobe_bulletin_board.domain.post.service;
+
+import com.devkobe24.kobe_bulletin_board.common.exception.CustomException;
+import com.devkobe24.kobe_bulletin_board.common.exception.ResponseCode;
+import com.devkobe24.kobe_bulletin_board.domain.post.model.request.ReadPostRequest;
+import com.devkobe24.kobe_bulletin_board.domain.post.model.response.ReadPostResponse;
+import com.devkobe24.kobe_bulletin_board.domain.repository.PostReadRepository;
+import com.devkobe24.kobe_bulletin_board.domain.repository.entity.Post;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PostReadService {
+
+	private final PostReadRepository postReadRepository;
+
+	@Transactional(transactionManager = "readPostTransactionManager")
+	public ReadPostResponse readPost(ReadPostRequest request) {
+		try {
+			Post post = postReadRepository.findPostByIdWithLock(request.getId())
+				.orElseThrow(() -> {
+					log.error("Post not found with id: {}", request.getId());
+					return new CustomException(ResponseCode.POST_NOT_EXISTS);
+				});
+
+			// 조회수 증가.
+			post.incrementViewCount();
+
+			// 변경사항 저장.
+			postReadRepository.save(post);
+
+			// 응답 생성
+			return new ReadPostResponse(ResponseCode.SUCCESS, post);
+		} catch (Exception e) {
+			log.error("Unexpected error while reading post: {}", e.getMessage());
+			throw new CustomException(ResponseCode.POST_READ_FAILED, e.getMessage());
+		}
+	}
+}
