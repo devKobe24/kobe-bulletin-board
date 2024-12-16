dd13e1cc33dc1cf0b3e883ee12a3b6c8af949cef
Kobe <dev.skyachieve91@gmail.com>
2024-12-16 16:38:42 +0900

    chore: PostRepository에서 PostCreateRepository로 변경.

--- src/main/java/com/devkobe24/kobe_bulletin_board/domain/post/service/PostCreateService.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/post/service/PostCreateService.java
@@ -4,7 +4,7 @@ import com.devkobe24.kobe_bulletin_board.common.exception.CustomException;
 import com.devkobe24.kobe_bulletin_board.common.exception.ResponseCode;
 import com.devkobe24.kobe_bulletin_board.domain.post.model.request.CreatePostRequest;
 import com.devkobe24.kobe_bulletin_board.domain.post.model.response.CreatePostResponse;
-import com.devkobe24.kobe_bulletin_board.domain.repository.PostRepository;
+import com.devkobe24.kobe_bulletin_board.domain.repository.PostCreateRepository;
 import com.devkobe24.kobe_bulletin_board.domain.repository.UserRepository;
 import com.devkobe24.kobe_bulletin_board.domain.repository.entity.Post;
 import com.devkobe24.kobe_bulletin_board.domain.repository.entity.User;
