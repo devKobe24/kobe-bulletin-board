a00a1b47ca6bb61e0bf1cad7cdac2fe145793ee8
Kobe <dev.skyachieve91@gmail.com>
2024-12-18 08:12:36 +0900

    feat: UpdatePostResponse 클래스 구현.

--- src/main/java/com/devkobe24/kobe_bulletin_board/domain/post/model/response/UpdatePostResponse.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/post/model/response/UpdatePostResponse.java
@@ -1,4 +1,10 @@
 package com.devkobe24.kobe_bulletin_board.domain.post.model.response;
 
+import com.devkobe24.kobe_bulletin_board.common.exception.ResponseCode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+@Schema(description = "게시물 수정 Response")
 public class UpdatePostResponse {
 }
