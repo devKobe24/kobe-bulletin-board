2002a606f15d652d7afbc19b2e9353f19d43b383
Kobe <dev.skyachieve91@gmail.com>
2024-12-18 08:08:41 +0900

    feat: findById 쿼리 메서드 추가.

--- src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/PostUpdateRepository.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/PostUpdateRepository.java
@@ -4,4 +4,5 @@ import com.devkobe24.kobe_bulletin_board.domain.repository.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostUpdateRepository extends JpaRepository<Post, Long> {
+	Optional<Post> findById(Long id);
 }
