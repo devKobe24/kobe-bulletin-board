e9c16e4c7e58b7187b7f731a2fc6345915a50583
Kobe <dev.skyachieve91@gmail.com>
2024-12-18 05:07:01 +0900

    feat: LogoutRequest record 생성 및 구현.

---
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/auth/model/request/LogoutRequest.java
@@ -0,0 +1,12 @@
+package com.devkobe24.kobe_bulletin_board.domain.auth.model.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+@Schema(description = "로그아웃 Request")
+public record LogoutRequest(
+
+	@Schema(description = "로그아웃할 사용자 인증 토큰", example = "Bearer <access_token>")
+	@NotBlank
+	String token
+) {}
