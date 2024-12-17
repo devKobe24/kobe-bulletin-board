5c195e1b31fd70ba9454a1159f6da9f42a287086
Kobe <dev.skyachieve91@gmail.com>
2024-12-18 05:13:20 +0900

    feat: PostUpdateRepository interface 생성.

---
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/PostUpdateRepository.java
@@ -0,0 +1,7 @@
+package com.devkobe24.kobe_bulletin_board.domain.repository;
+
+import com.devkobe24.kobe_bulletin_board.domain.repository.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostUpdateRepository extends JpaRepository<Post, Long> {
+}
