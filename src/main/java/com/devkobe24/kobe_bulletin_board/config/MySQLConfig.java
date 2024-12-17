08d80b984e1070c3386ebd7bfa1812851d78caac
Kobe <dev.skyachieve91@gmail.com>
2024-12-18 05:03:21 +0900

    feat: loginTransactionManager 생성 및 구현.

--- src/main/java/com/devkobe24/kobe_bulletin_board/config/MySQLConfig.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/config/MySQLConfig.java
@@ -42,6 +42,12 @@ public class MySQLConfig {
 		return manager;
 	}
 
+	@Bean(name = "loginTransactionManager")
+	public PlatformTransactionManager loginTransactionManager(DataSource dataSource) {
+		DataSourceTransactionManager manager = new DataSourceTransactionManager(dataSource);
+		return manager;
+	}
+
 	@Bean(name = "createPostTransactionManager")
 	public PlatformTransactionManager createPostTransactionManager(DataSource dataSource) {
 		DataSourceTransactionManager manager = new DataSourceTransactionManager(dataSource);
