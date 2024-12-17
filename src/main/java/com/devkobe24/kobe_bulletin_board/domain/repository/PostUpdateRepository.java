f71b5713ab6be57957141598813ceb819c8731d7
Kobe <dev.skyachieve91@gmail.com>
2024-12-18 08:09:10 +0900

    feat: java.util.Optional import

--- src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/PostUpdateRepository.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/PostUpdateRepository.java
@@ -3,6 +3,8 @@ package com.devkobe24.kobe_bulletin_board.domain.repository;
 import com.devkobe24.kobe_bulletin_board.domain.repository.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostUpdateRepository extends JpaRepository<Post, Long> {
 	Optional<Post> findById(Long id);
 }
