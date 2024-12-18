6902bcb231d28c15771e8f18fd1ecc6412a07612
Kobe <dev.skyachieve91@gmail.com>
2024-12-19 05:49:15 +0900

    feat: dotenv 적용을 위한 코드 추가

--- src/main/java/com/devkobe24/kobe_bulletin_board/KobeBulletinBoardApplication.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/KobeBulletinBoardApplication.java
@@ -1,5 +1,6 @@
 package com.devkobe24.kobe_bulletin_board;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KobeBulletinBoardApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(KobeBulletinBoardApplication.class, args);
 	}
 
