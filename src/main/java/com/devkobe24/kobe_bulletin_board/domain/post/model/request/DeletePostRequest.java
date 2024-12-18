7190bff0ec2536eae7fde66429f5febcef3c3dea
Kobe <dev.skyachieve91@gmail.com>
2024-12-18 12:52:09 +0900

    feat: DeletePostRequest 클래스 생성 및 구현.

---
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/post/model/request/DeletePostRequest.java
@@ -0,0 +1,18 @@
+package com.devkobe24.kobe_bulletin_board.domain.post.model.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Schema(description = "게시물 삭제 Request")
+public class DeletePostRequest {
+
+	@Schema(description = "삭제할 게시물 id")
+	@NotBlank
+	@NotNull
+	Long id;
+}
