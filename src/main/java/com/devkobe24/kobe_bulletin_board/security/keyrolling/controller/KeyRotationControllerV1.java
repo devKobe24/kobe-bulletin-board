150df4791e846a8c7e73042305fb0c26cd4a002f
Kobe <dev.skyachieve91@gmail.com>
2024-12-20 05:42:27 +0900

    feat: KeyRotationControllerV1 클래스 생성 및 구현.

---
+++ src/main/java/com/devkobe24/kobe_bulletin_board/security/keyrolling/controller/KeyRotationControllerV1.java
@@ -0,0 +1,31 @@
+package com.devkobe24.kobe_bulletin_board.security.keyrolling.controller;
+
+import com.devkobe24.kobe_bulletin_board.security.keyrolling.model.response.CreateRefreshSecretKeyResponse;
+import com.devkobe24.kobe_bulletin_board.security.keyrolling.model.response.CreateSecretKeyResponse;
+import com.devkobe24.kobe_bulletin_board.security.keyrolling.service.KeyRotationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+public class KeyRotationControllerV1 {
+
+	private final KeyRotationService keyRotationService;
+
+	@PostMapping("/rotate-keys")
+	public CreateSecretKeyResponse rotateKeys() {
+		String activeKey = keyRotationService.rotateSecretKey();
+
+		return new CreateSecretKeyResponse(activeKey);
+	}
+
+	@PostMapping("/rotate-refreshKey")
+	public CreateRefreshSecretKeyResponse rotateRefreshKey() {
+		String activeKey = keyRotationService.rotateRefreshSecretKey();
+
+		return new CreateRefreshSecretKeyResponse(activeKey);
+	}
+}
