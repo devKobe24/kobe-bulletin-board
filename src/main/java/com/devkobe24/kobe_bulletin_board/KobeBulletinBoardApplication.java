b4e0300b9e32a46720f0d1026044134ac50e0ce7
Kobe <dev.skyachieve91@gmail.com>
2024-12-20 05:44:45 +0900

    fix: KobeBulletinBoardApplication 내부 dotenv 로직 추가.

--- src/main/java/com/devkobe24/kobe_bulletin_board/KobeBulletinBoardApplication.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/KobeBulletinBoardApplication.java
@@ -1,17 +1,26 @@
 package com.devkobe24.kobe_bulletin_board;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class KobeBulletinBoardApplication {
 
 	public static void main(String[] args) {
+		// Dotenv로 환경 변수 로드
 		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
-		SpringApplication.run(KobeBulletinBoardApplication.class, args);
+		// Dotenv 값을 Map으로 변환
+		Map<String, Object> properties = new HashMap<>();
+		dotenv.entries().forEach(entry -> properties.put(entry.getKey(), entry.getValue()));
+
+		// SpringApplication 실행
+		new SpringApplicationBuilder(KobeBulletinBoardApplication.class)
+			.properties(properties)
+				.run(args);
 	}
-
 }
