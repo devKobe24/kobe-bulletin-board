477531afcc31a720af4e21101437ccae06b8ac74
Kobe <dev.skyachieve91@gmail.com>
2024-12-20 08:43:48 +0900

    fix: HealthCheckController package 이동

--- src/main/java/com/devkobe24/kobe_bulletin_board/HealthCheckController.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/HealthCheckController.java
@@ -1,6 +1,5 @@
-package com.devkobe24.kobe_bulletin_board.health;
+package com.devkobe24.kobe_bulletin_board;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
