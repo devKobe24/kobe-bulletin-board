795a8decb23c841c15ce2a9fd589d062b28f306e
Kobe <dev.skyachieve91@gmail.com>
2024-12-21 05:45:05 +0900

    feat: findAll 유저 전체 리스리 쿼리 메서드 추가.

--- src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/UserRepository.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.devkobe24.kobe_bulletin_board.domain.repository.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	boolean existsByNickName(String nickName);
 	Optional<User> findByNickName(String nickName);
 	Optional<User> findById(Long id);
+	List<User> findAll();
 }
