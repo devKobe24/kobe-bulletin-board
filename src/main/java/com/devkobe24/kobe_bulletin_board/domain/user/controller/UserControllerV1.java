7c3b6733d63afefe9a56584713fe42763fbc7b0e
Kobe <dev.skyachieve91@gmail.com>
2024-12-21 03:36:03 +0900

    feat: UserControllerV1 생성 및 구현.

---
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/user/controller/UserControllerV1.java
@@ -0,0 +1,45 @@
+package com.devkobe24.kobe_bulletin_board.domain.user.controller;
+
+import com.devkobe24.kobe_bulletin_board.domain.user.model.request.CreateUserRequest;
+import com.devkobe24.kobe_bulletin_board.domain.user.model.request.ReadSpecificUserRequest;
+import com.devkobe24.kobe_bulletin_board.domain.user.model.response.CreateUserResponse;
+import com.devkobe24.kobe_bulletin_board.domain.user.model.response.ReadSpecificUserResponse;
+import com.devkobe24.kobe_bulletin_board.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "User API", description = "V1 User API")
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserControllerV1 {
+
+	private final UserService userService;
+
+	@Operation(
+		summary = "새로운 유저를 생성합니다.",
+		description = "새로운 유저의 정보를 생성하여 저장합니다."
+	)
+	@PostMapping("/create-user")
+	public CreateUserResponse createUser(
+		@RequestBody @Valid CreateUserRequest request
+	) {
+		return userService.createUser(request);
+	}
+
+	@Operation(
+		summary = "특정 유저를 조회합니다.",
+		description = "ID로 특정 유저를 조회합니다."
+	)
+	@GetMapping("{id}")
+	public ReadSpecificUserResponse readSpecificUser(@PathVariable("id") Long id) {
+		ReadSpecificUserRequest request = new ReadSpecificUserRequest(id);
+
+		ReadSpecificUserResponse response = userService.readSpecificUser(request);
+
+		return response;
+	}
+}
