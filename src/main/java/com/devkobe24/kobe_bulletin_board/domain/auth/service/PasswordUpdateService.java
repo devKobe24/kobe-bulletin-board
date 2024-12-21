6b66cd2f1da9cc9c11cbdf534c613c9f5dc108c2
Kobe <dev.skyachieve91@gmail.com>
2024-12-22 07:02:18 +0900

    feat: PasswordUpdateService 생성 및 구현. - 비밀번호 변경 서비스 로직 생성 및 구현.

---
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/auth/service/PasswordUpdateService.java
@@ -0,0 +1,42 @@
+package com.devkobe24.kobe_bulletin_board.domain.auth.service;
+
+import com.devkobe24.kobe_bulletin_board.common.exception.CustomException;
+import com.devkobe24.kobe_bulletin_board.common.exception.ResponseCode;
+import com.devkobe24.kobe_bulletin_board.domain.auth.model.request.UpdatePasswordRequest;
+import com.devkobe24.kobe_bulletin_board.domain.auth.model.response.UpdatePasswordResponse;
+import com.devkobe24.kobe_bulletin_board.domain.repository.UserRepository;
+import com.devkobe24.kobe_bulletin_board.domain.repository.entity.User;
+import com.devkobe24.kobe_bulletin_board.security.Hasher;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PasswordUpdateService {
+
+	private final UserRepository userRepository;
+	private final Hasher hasher;
+
+	@Transactional(transactionManager = "updatePasswordTransactionManager")
+	public UpdatePasswordResponse updatePassword(UpdatePasswordRequest request) {
+		User user = userRepository.findById(request.getId())
+			.orElseThrow(() -> new CustomException(ResponseCode.USER_NOT_EXISTS));
+
+		// 비밀번호 일치여부 확인
+		String hashedValue = hasher.getHashingValue(request.getPreviousPassword());
+		if (!user.getUserCredentials().getHashedPassword().equals(hashedValue)) {
+			throw new CustomException(ResponseCode.MISS_MATCH_PASSWORD);
+		}
+
+		String newHashedValue = hasher.getHashingValue(request.getNewPassword());
+
+		// 사용자 비밀번호 업데이트
+		user.getUserCredentials().setHashedPassword(newHashedValue);
+		userRepository.save(user); // 변경사항 저장
+
+		return new UpdatePasswordResponse(ResponseCode.SUCCESS);
+	}
+}
