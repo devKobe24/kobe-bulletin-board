e101f7a7cf10058b85c0cbfc389d5f38eb0a6006
Kobe <dev.skyachieve91@gmail.com>
2024-12-22 06:43:33 +0900

    feat: ModifyPasswordRequest 도메인 클래스 추가.

---
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/auth/model/request/ModifyPasswordRequest.java
@@ -0,0 +1,17 @@
+package com.devkobe24.kobe_bulletin_board.domain.auth.model.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+@Schema(description = "비밀번호 변경 Request")
+public class ModifyPasswordRequest {
+	@Schema(description = "유저 ID", example = "1")
+	private Long id;
+
+	@Schema(description = "변경 전 유저 비밀번호")
+	private String previousPassword;
+
+	@Schema(description = "변경 할 유저 비밀번호")
+	private String newPassword;
+}
