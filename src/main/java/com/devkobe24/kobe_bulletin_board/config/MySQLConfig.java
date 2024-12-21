3fa5e9ae55a8b6ccd9bf9eb859d6f4d9e8f6f2ea
Kobe <dev.skyachieve91@gmail.com>
2024-12-22 06:42:43 +0900

    feat: modifyPasswordTransactionManager PlatformTransactionManager 생성 및 구현.

--- src/main/java/com/devkobe24/kobe_bulletin_board/config/MySQLConfig.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/config/MySQLConfig.java
@@ -78,6 +78,12 @@ public class MySQLConfig {
 		return manager;
 	}
 
+	@Bean(name = "modifyPasswordTransactionManager")
+	public PlatformTransactionManager modifyPasswordTransactionManager(DataSource dataSource) {
+		DataSourceTransactionManager manager = new DataSourceTransactionManager(dataSource);
+		return manager;
+	}
+
 	@Bean(name = "createPostTransactionManager")
 	public PlatformTransactionManager createPostTransactionManager(DataSource dataSource) {
 		DataSourceTransactionManager manager = new DataSourceTransactionManager(dataSource);
