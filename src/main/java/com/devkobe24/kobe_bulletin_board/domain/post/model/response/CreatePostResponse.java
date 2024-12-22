6227c23e925030f60128b8b7db6cf01c0a6196c8
Kobe <dev.skyachieve91@gmail.com>
2024-12-23 07:10:57 +0900

    feat: CreatePostResponse Response 값 추가. - token

--- src/main/java/com/devkobe24/kobe_bulletin_board/domain/post/model/response/CreatePostResponse.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/post/model/response/CreatePostResponse.java
@@ -1,6 +1,7 @@
 package com.devkobe24.kobe_bulletin_board.domain.post.model.response;
 
 import com.devkobe24.kobe_bulletin_board.common.exception.ResponseCode;
+import com.devkobe24.kobe_bulletin_board.domain.repository.entity.PostCredentials;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
@@ -14,8 +15,12 @@ public class CreatePostResponse {
 	@Schema(description = "응답 메시지")
 	private final String message;
 
-	public CreatePostResponse(ResponseCode responseCode) {
+	@Schema(description = "게시물 토큰")
+	private final PostCredentials token;
+
+	public CreatePostResponse(ResponseCode responseCode, PostCredentials token) {
 		this.code = responseCode.getCode();
 		this.message = responseCode.getMessage();
+		this.token = token;
 	}
 }
