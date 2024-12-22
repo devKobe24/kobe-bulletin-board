d8c38bed1d0235f477516275b6696d67a7f9c00d
Kobe <dev.skyachieve91@gmail.com>
2024-12-23 06:06:53 +0900

    feat: PostCredentials 엔티티 생성 및 구현.

---
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/entity/PostCredentials.java
@@ -0,0 +1,35 @@
+package com.devkobe24.kobe_bulletin_board.domain.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "post_credentials")
+public class PostCredentials {
+
+	@Id
+	@Column(name = "post_ic")
+	private Long postId;
+
+	@OneToOne
+	@MapsId
+	@JoinColumn(name = "post_id", referencedColumnName = "id")
+	private Post post;
+
+	@Column(name = "hashed_password", length = 60, nullable = false)
+	private String hashedPassword;
+
+	@Column(name = "token", nullable = false)
+	private String token;
+
+	@Column(name = "is_revoked", nullable = false)
+	private boolean isRevoked;
+
+	@Column(name = "is_expired", nullable = false)
+	private boolean isExpired;
+}
