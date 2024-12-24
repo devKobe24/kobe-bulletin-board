48a42c28d544a8e311998370d202983856d87bb1
Kobe <dev.skyachieve91@gmail.com>
2024-12-24 16:29:20 +0900

    chore: userCredentialsRepository, authService 추가.

--- src/main/java/com/devkobe24/kobe_bulletin_board/domain/user/service/UserService.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/user/service/UserService.java
@@ -27,9 +27,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserService {
 
+	private final UserCredentialsRepository userCredentialsRepository;
 	private final UserRepository userRepository;
 	private final CommonService<User, Long> userCommonService;
 	private final Hasher hasher;
+	private final AuthService authService;
 
 	@Transactional(transactionManager = "createUserTransactionManager")
 	public CreateUserResponse createUser(CreateUserRequest request) {
