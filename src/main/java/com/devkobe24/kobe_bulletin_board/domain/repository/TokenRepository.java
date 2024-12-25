850ef680cb5188ac4a288a5b18d6ef19f12012c9
Kobe <dev.skyachieve91@gmail.com>
2024-12-25 12:28:58 +0900

    delete: import UserRole, Modifying 삭제.

--- src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/TokenRepository.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/TokenRepository.java
@@ -1,9 +1,7 @@
 package com.devkobe24.kobe_bulletin_board.domain.repository;
 
-import com.devkobe24.kobe_bulletin_board.common.role.UserRole;
 import com.devkobe24.kobe_bulletin_board.domain.repository.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
