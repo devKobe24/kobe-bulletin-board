bf93539bb6d76b83fb0ec75f182ac16aa5c500e9
Kobe <dev.skyachieve91@gmail.com>
2024-12-27 07:29:11 +0900

    chore: 불필요 주석 삭제.

--- src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/PostCredentialRepository.java
+++ src/main/java/com/devkobe24/kobe_bulletin_board/domain/repository/PostCredentialRepository.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 
 public interface PostCredentialRepository extends JpaRepository<PostCredentials, Long> {
 
-	// 특정 사용자의 유효한 토큰을 찾을 수 있습니다.
 	@Query("SELECT p " +
 		"FROM PostCredentials p " +
 		"WHERE p.post.id = :postId " +
